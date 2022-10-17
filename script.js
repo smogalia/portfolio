5f9a1ab8fff8f36d98877110f87ff0e70e1edca1
smogalia <94159258+smogalia@users.noreply.github.com>
2022-10-17 12:30:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const navbar = document.getElementById("navbar-portfolio");
+const scrollTopButton = document.getElementById("scroll-top-button");
+
+window.onscroll=function(){
+    if (window.pageYOffset>200) {
+        navbar.classList.remove("bg-transparent","navbar-dark");
+        navbar.classList.add("bg-light","navbar-light");
+        scrollTopButton.classList.add("show");
+    }
+    else{
+        navbar.classList.add("bg-transparent","navbar-dark");
+        navbar.classList.remove("bg-light","navbar-light");
+        scrollTopButton.classList.remove("show");
+    }
+}
+
+// Get the root element
+var r = document.querySelector('body');
+// Create a function for setting a variable value
+
+function changeColor(color,colorhover) {
+    // change les variables
+    r.style.setProperty('--color-accent', color);
+    r.style.setProperty('--color-accent-hover', colorhover);
+  }
+
+  function ChangeColorFromPicker(){
+    let color = document.getElementById("colorAccent");
+    let colorHover = document.getElementById("colorHover");
+
+    changeColor(color.value,colorHover.value);
+
+  }
+
+/* Create a function for getting a variable value
+function myFunction_get() {
+  // Get the styles (properties and values) for the root
+  var rs = getComputedStyle(r);
+  // Alert the value of the --blue variable
+  alert("The value of --blue is: " + rs.getPropertyValue('--red-color'));
+}*/
+
+function ShowPanelColor(){
+    let panel = document.getElementById('color-setter');
+    if(panel.style.left=="0px"){
+        panel.style.left = "-100px";
+    }
+    else{
+        panel.style.left = "0px";
+    }
+    
+}
